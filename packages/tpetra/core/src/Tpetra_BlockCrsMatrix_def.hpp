9dcffa75b54829fd6004571a6c07f73f96fd72a0
tcclevenger <tccleve@sandia.gov>
2022-07-19 15:07:21 -0600

    remove one

--- packages/tpetra/core/src/Tpetra_BlockCrsMatrix_def.hpp
+++ packages/tpetra/core/src/Tpetra_BlockCrsMatrix_def.hpp
@@ -2774,7 +2774,7 @@ public:
     {
       using policy_type = Kokkos::TeamPolicy<host_exec>;
       const auto policy = policy_type (numImportLIDs, 1, 1)
-        .set_scratch_size (0, Kokkos::PerTeam (sizeof (GO) * maxRowNumEnt +
+        .set_scratch_size (1, Kokkos::PerTeam (sizeof (GO) * maxRowNumEnt +
                                                sizeof (LO) * maxRowNumEnt +
                                                numBytesPerValue * maxRowNumScalarEnt));
 
@@ -2795,11 +2795,11 @@ public:
           const size_t i = member.league_rank();
 
           Kokkos::View<GO*, host_scratch_space> gblColInds
-            (member.team_scratch (0), maxRowNumEnt);
+            (member.team_scratch (1), maxRowNumEnt);
           Kokkos::View<LO*, host_scratch_space> lclColInds
-            (member.team_scratch (0), maxRowNumEnt);
+            (member.team_scratch (1), maxRowNumEnt);
           Kokkos::View<impl_scalar_type*, host_scratch_space> vals
-            (member.team_scratch (0), maxRowNumScalarEnt);
+            (member.team_scratch (1), maxRowNumScalarEnt);
 
 //          Kokkos::View<GO*, host_exec> gblColInds
 //            ("", maxRowNumEnt);
